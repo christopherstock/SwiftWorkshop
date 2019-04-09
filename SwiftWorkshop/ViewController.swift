1669d00def03c6a2f486d1858a93ffea9062e4f9
Christopher Stock <christopher.stock@mayflower.de>
2019-04-09 16:39:29 +0200

    Added app sceleton. Added window title.

---
+++ SwiftWorkshop/ViewController.swift
@@ -0,0 +1,30 @@
+
+    import Cocoa
+
+    /**
+        The native ViewController.
+    */
+    class ViewController : NSViewController
+    {
+        /** The application title. */
+        static let APP_TITLE = "Swift / SpriteKit Workshop, (c) 2019 Mayflower GmbH"
+
+        /**
+            Being invoked after the view has been loaded.
+            Sets up the view after being loaded.
+        */
+        override func viewDidLoad() -> Void
+        {
+            super.viewDidLoad()
+        }
+
+        /**
+            Being invoked after the view has appeared on the screen.
+        */
+        override func viewDidAppear() -> Void
+        {
+            super.viewDidAppear()
+
+            self.view.window?.title = ViewController.APP_TITLE
+        }
+    }
