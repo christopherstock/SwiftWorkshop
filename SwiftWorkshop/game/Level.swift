ec682796bccff021e19ca6136b58caa349c3b410
Christopher Stock <christopher.stock@mayflower.de>
2019-04-18 13:49:04 +0200

    Added some GameObjects to the Level.

--- SwiftWorkshop/game/Level.swift
+++ SwiftWorkshop/game/Level.swift
@@ -6,6 +6,18 @@
     ///
     class Level
     {
+        /// Non collidable decoration.
+        private var decos  :[GameObject]
+        /// Collidable static walls.
+        private var walls  :[GameObject]
+        /// Collidable dynamic boxes objects.
+        private var boxes  :[GameObject]
+        /// Collectable game items.
+        private var items  :[GameObject]
+
+        /// The game object being controlled by the user.
+        private var player :GameObject
+
         ///
         /// Creates a new instance of the game level.
         ///
@@ -13,5 +25,16 @@
         ///
         init( scene: SKScene )
         {
+            decos = []
+            walls = []
+            boxes = []
+            items = []
+
+            decos.append( GameObject( scene: scene, x: 0,    y: 0,   imageFile: "foreground.png" ) )
+            walls.append( GameObject( scene: scene, x: 500,  y: 0,   imageFile: "stone1.png"     ) )
+            boxes.append( GameObject( scene: scene, x: 300,  y: 0,   imageFile: "box1.png"       ) )
+            items.append( GameObject( scene: scene, x: 300,  y: 250, imageFile: "raspberry.png"  ) )
+
+            player = GameObject( scene: scene, x: 150, y: 0, imageFile: "walkRight.png" )
         }
     }
